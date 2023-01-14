1531ba097ca44e1c92d977582335f6890874f77f
Kamila <kamila.matuszak@list.pl>
2023-01-14 11:23:56 +0100

    Simple enemy path following

--- TowerDefense/Assets/Scripts/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed;
+    
+    private Transform[] waypoints;
+    private int waypoint = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        waypoints = LevelManager.Instance.GetWaypoints().waypoints;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waypoint == waypoints.Length)
+        {
+            Debug.Log("Enemy reached the end");
+            Destroy(gameObject);
+            waypoint = 0;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypoint].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, waypoints[waypoint].position) < 0.01f)
+        {
+            waypoint++;
+        }
     }
 }
--- TowerDefense/Assets/Scripts/LevelManager.cs
+++ TowerDefense/Assets/Scripts/LevelManager.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance { get; private set; }
+    
     private int wave = 0;
     private int money = 0;
-    
+
+    private Waypoints waypoints;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = GetComponentInChildren<Waypoints>();
     }
 
     // Update is called once per frame
@@ -38,5 +49,10 @@ public class LevelManager : MonoBehaviour
     public int GetMoney()
     {
         return money;
+    }
+
+    public Waypoints GetWaypoints()
+    {
+        return waypoints;
     }
 }
